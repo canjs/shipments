cae0029cf59aad4784e27d354a55be593de30537
Justin Meyer <justinbmeyer@gmail.com>
2021-08-25 14:50:08 -0500

    removing shipments code

--- models/shipment.js
+++ models/shipment.js
@@ -3,21 +3,15 @@ import Organization from "./organization";
 
 const OrgHydateType = {
 	type: function(value){
-		return Organization.connection.hydrateInstance(value)
+		return //??
 	},
 	serialize: false
 };
 window.Organization = Organization;
 
 const Shipment = DefineMap.extend("Shipment",{
-	id: { type: "number", identity: true },
-	name: "string",
-	destinationOrganizationId: {type: "string"},
 	destinationOrganization: OrgHydateType,
-	originOrganizationId: {type: "string"},
 	originOrganization: OrgHydateType,
-	departureDate: {type: "date"},
-	arrivalDate: {type: "date"}
 });
 
 Shipment.List = DefineList.extend("ShipmentList",{
@@ -29,19 +23,6 @@ const baseConnection = realtimeRestModel({
 	List: Shipment.List,
 	url: "/api/shipments/{id}"
 });
-const oldUpdatedInstance = baseConnection.updatedInstance;
-baseConnection.updatedInstance = function(instance, data){
-	const result = oldUpdatedInstance.call(this, instance, data);
-	if(instance.destinationOrganization) {
-		instance.destinationOrganization =
-			Organization.connection.instanceStore.get(instance.destinationOrganizationId);
-	}
-	if(instance.originOrganization) {
-		instance.originOrganization =
-			Organization.connection.instanceStore.get(instance.originOrganizationId);
-	}
-	return result;
-}
 
 Shipment.connection = baseConnection
 
--- pages/shipments/shipments.js
+++ pages/shipments/shipments.js
@@ -12,86 +12,8 @@ const formater = new Intl.DateTimeFormat('default', {
 const PageShipments = Component.extend({
 	tag: "page-shipments",
 	view: shipmentsStache,
-	ViewModel: {
-		sort: "string",
-		originOrganizationId: "string",
+	ViewModel: {}
 
-		// this is needed for filtering
-		allShipmentsPromise: {
-			default() {
-				return Shipment.getList({include: ["destinationOrganization","originOrganization"]});
-			}
-		},
-		shipmentsPromise: {
-			get(){
-				let requestingEverything = true;
-				var query = {filter: { }, include: ["destinationOrganization","originOrganization"]};
-				if(this.sort) {
-					requestingEverything = false;
-					query.sort =  this.sort;
-				}
-				if(this.originOrganizationId) {
-					requestingEverything = false;
-					query.filter.originOrganizationId = parseInt( this.originOrganizationId, 10);
-				}
-				if(requestingEverything) {
-					return this.allShipmentsPromise;
-				} else {
-					return Shipment.getList(query);
-				}
-
-			}
-		},
-		organizations: {
-			value({listenTo, resolve, stopListening}){
-				this.allShipmentsPromise.then((shipments) => {
-
-					const destinations = shipments.map( shipment => shipment.destinationOrganization );
-					const origins = shipments.map( shipment => shipment.originOrganization );
-					
-					resolve({
-						destinations: Array.from( new Set(destinations) ),
-						origins: Array.from( new Set(origins) ),
-					});
-				});
-			}
-		},
-		formatDate(date) {
-			return formater.format(date);
-		},
-		_editingShipment: type.maybe(Shipment),
-		isEditing(org){
-			return org === this._editingShipment;
-		},
-		edit(shipment){
-			this._editingShipment = shipment;
-		},
-		cancelEdit(){
-			this._editingShipment = null;
-		},
-		get editShipmentComponent(){
-			if(this._editingShipment) {
-				return new ShipmentEdit({
-					viewModel: {
-						shipment: this._editingShipment,
-						organizations: this.organizations,
-						onSaved: this.cancelEdit.bind(this),
-						onCancelled: this.cancelEdit.bind(this)
-					}
-				})
-			}
-		},
-		connectedCallback(){
-			this.listenTo("editShipmentComponent", ({value, oldValue})=> {
-				if(oldValue) {
-					BitModal.remove(oldValue);
-				}
-				if(value) {
-					BitModal.add(value);
-				}
-			});
-		}
-	}
 });
 
 export default PageShipments;
